31e49147784e3c20a88a63b2c44cc266a5bcb698
alexandrafs5 <alexandra051009@gmail.com>
2024-05-26 22:55:59 -0700

    Optimize the win selection

--- script.js
+++ script.js
@@ -25,30 +25,14 @@ function playGame() {
     let computerScore = 0;
 
     function playRound(humanChoice, computerChoice) {
-        if (computerChoice === "Rock" && humanChoice === "Rock") {
+        if (computerChoice === humanChoice) {
             console.log("That's a draw!");
-        } else if (computerChoice === "Rock" && humanChoice === "Paper") {
+        } else  if ((computerChoice === "Rock" && humanChoice === "Paper") || (computerChoice === "Paper" && humanChoice === "Scissors") || (computerChoice === "Scissors" && humanChoice === "Rock")) {
             console.log("You win!");
             humanScore += 1;
-        } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+        } else if ((computerChoice === "Rock" && humanChoice === "Scissors") || (computerChoice === "Paper" && humanChoice === "Rock") || (computerChoice === "Scissors" && humanChoice === "Paper")) {
             console.log("You lose!");
             computerScore += 1;
-        } else if (computerChoice === "Paper" && humanChoice === "Rock") {
-            console.log("You lose!");
-            computerScore += 1;
-        } else if (computerChoice === "Paper" && humanChoice === "Paper") {
-            console.log("That's a draw!");
-        } else if (computerChoice === "Paper" && humanChoice === "Scissors") {
-            console.log("You win!");
-            humanScore += 1;
-        } else if (computerChoice === "Scissors" && humanChoice === "Rock") {
-            console.log("You win!");
-            humanScore += 1;
-        } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
-            console.log("You lose!");
-            computerScore += 1;
-        } else if (computerChoice === "Scissors" && humanChoice === "Scissors") {
-            console.log("That's a draw!");
         }
     }
 
